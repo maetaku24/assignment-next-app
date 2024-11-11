906ef97ce1d580e154edacff2b56ca42579df148
maetaku24 <yataku24@gmail.com>
2024-11-11 16:37:05 +0900

    ⚡️ 課題実装

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import Image from "next/image";
+import classes from "../../../styles/Detail.module.css";
+import { Post } from "@/interfaces/interfaces";
+
+const Detail: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [post, setPost] = useState<Post | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetcher = async () => {
+      setLoading(true);
+      const res = await fetch(
+        `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`
+      );
+      const { post } = await res.json();
+      setPost(post);
+      setLoading(false);
+    };
+
+    fetcher();
+  }, [id]);
+
+  if (loading) return <div className={classes.postloading}>読み込み中...</div>;
+  if (!post)
+    return (
+      <div className={classes.postError}>記事が見つかりませんでした。</div>
+    );
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.post}>
+        <div className={classes.postImage}>
+          <Image height={1000} width={1000} src={post.thumbnailUrl} alt=""  />
+        </div>
+        <div className={classes.postContent}>
+          <div className={classes.postInfo}>
+            <div className={classes.postDate}>
+              {new Date(post.createdAt).toLocaleDateString()}
+            </div>
+            <div className={classes.postCategories}>
+              {post.categories.map((category, id) => {
+                return (
+                  <p key={id} className={classes.postCategory}>
+                    {category}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+          <p className={classes.postTitle}>{post.title}</p>
+          <div
+            className={classes.postBody}
+            dangerouslySetInnerHTML={{ __html: post.content || "" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;

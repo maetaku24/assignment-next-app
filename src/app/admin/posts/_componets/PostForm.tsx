780b660dd262065bed88a9aa30f12f89fce0c051
maetaku24 <yataku24@gmail.com>
2024-12-05 16:02:36 +0900

    【追加】posts、categorieディレクト下にコンポーネントを追加

---
+++ src/app/admin/posts/_componets/PostForm.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Category } from "@/app/_interfaces/Categories";
+import { CategoriesSelect } from "../../../_componets/CategoriesSelect";
+
+interface Props {
+  mode: "new" | "edit";
+  title: string;
+  setTitle: (title: string) => void;
+  content: string;
+  setContent: (content: string) => void;
+  thumbnailUrl: string;
+  setThumbnailUrl: (thumbnailUrl: string) => void;
+  categories: Category[];
+  setCategories: (categories: Category[]) => void;
+  onSubmit: (e: React.FormEvent) => void;
+  onDelete?: () => void;
+}
+
+export const PostForm: React.FC<Props> = ({
+  mode,
+  title,
+  setTitle,
+  content,
+  setContent,
+  thumbnailUrl,
+  setThumbnailUrl,
+  categories,
+  setCategories,
+  onSubmit,
+  onDelete,
+}) => {
+  const router = useRouter();
+
+  return (
+    <form onSubmit={onSubmit} className="spece-y-4">
+      <div className="mt-4">
+        <label
+          htmlFor="title"
+          className="block text-sm font-bold text-gray-700"
+        >
+          タイトル
+        </label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="mt-1 block w-full rounded-md border border-gray-200 p-3"
+        />
+      </div>
+      <div className="mt-4">
+        <label
+          htmlFor="content"
+          className="block text-sm font-bold text-gray-700"
+        >
+          内容
+        </label>
+        <textarea
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="mt-1 block w-full rounded-md border border-gray-200 p-3"
+        />
+      </div>
+      <div className="mt-4">
+        <label
+          htmlFor="thumbnailUrl"
+          className="block text-sm font-bold text-gray-700"
+        >
+          サムネイルURL
+        </label>
+        <input
+          type="text"
+          id="thumbnailUrl"
+          value={thumbnailUrl}
+          onChange={(e) => setThumbnailUrl(e.target.value)}
+          className="mt-1 block w-full rounded-md border border-gray-200 p-3"
+        />
+      </div>
+      <div className="mt-4">
+        <label
+          htmlFor="thumbnailUrl"
+          className="block text-sm font-bold text-gray-700"
+        >
+          カテゴリー
+        </label>
+        <CategoriesSelect
+          selectedCategories={categories}
+          setSelectedCategories={setCategories}
+        />
+      </div>
+      <div className="flex justify-between">
+        <div className="flex">
+          <button
+            type="submit"
+            className="shadow-md bg-blue-500 hover:bg-blue-400 text-white rounded px-4 py-2 mt-4"
+          >
+            {mode === "new" ? "作成" : "更新"}
+          </button>
+          {mode === "edit" && (
+            <button
+              type="button"
+              className="shadow-md bg-red-500 hover:bg-red-400 text-white rounded px-4 py-2 mt-4 ml-2"
+              onClick={onDelete}
+            >
+              削除
+            </button>
+          )}
+        </div>
+        <button
+          type="button"
+          onClick={() => router.replace("/admin/posts")}
+          className="shadow-md bg-black hover:bg-gray-800 text-white rounded px-4 py-2 mt-4"
+        >
+          記事一覧へ戻る
+        </button>
+      </div>
+    </form>
+  );
+};

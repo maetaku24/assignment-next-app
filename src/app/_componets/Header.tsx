22b07b4468473b8e45a2fc3032f7d655633030e4
maetaku24 <yataku24@gmail.com>
2024-12-17 17:17:10 +0900

    【追加】ログイン・ログアウトメニュー追加

--- src/app/_componets/Header.tsx
+++ src/app/_componets/Header.tsx
@@ -1,15 +1,44 @@
+"use client";
+
 import Link from "next/link";
-import classes from "../_styles/Header.module.css";
+import React from "react";
+import { useSupabaseSession } from "../_hooks/useSupabaseSession";
+import { supabase } from "@/utils/supabase";
 
 export const Header: React.FC = () => {
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    window.location.href = "/";
+  };
+
+  const { session, isLoding } = useSupabaseSession();
+
   return (
-    <header className={classes.header}>
-      <Link href="/" className={classes.headerLink}>
-        BLOG
+    <header className="bg-gray-800 text-white p-6 font-bold flex justify-between items-center">
+      <Link href="/" className="header-link">
+        Blog
       </Link>
-      <Link href="/contact" className={classes.headerLink}>
-        お問い合わせ
-      </Link>
+      {!isLoding && (
+        <div className="flex items-center gap-4">
+          {session ? (
+            <>
+              <Link href="/admin" className="header-link">
+                管理画面
+              </Link>
+              <button onClick={handleLogout}>ログアウト</button>
+            </>
+          ) : (
+            <>
+              <Link href="/contact" className="header-link">
+                お問い合わせ
+              </Link>
+              <Link href="/login" className="header-link">
+                ログイン
+              </Link>
+            </>
+          )}
+        </div>
+      )}
     </header>
   );
 };
